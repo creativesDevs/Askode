5c6263c33a5b170f257b57797a73458a625ef6d2
FelixRG <correofrg@gmail.com>
2024-07-26 13:30:03 +0200

    created routes and views for Home and Game

---
+++ views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1 class="text-black">Home</h1>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+
+    },
+}
+</script>
+
+<style ></style>
---
+++ views/Quiz.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h1 class="text-black">Quiz</h1>
+</div>
+</template>
+
+  
+<script>
+export default {
+    name: 'Quiz',
+    data() {
+
+    },
+}
+</script>
+
+  
+<style >
+  </style>
