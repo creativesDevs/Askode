6e562bd3c0be754e93f8b66c34292b21e2ae3002
FelixRG <correofrg@gmail.com>
2024-07-27 12:29:30 +0200

    created a template to the view NotFound

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-black">NotFound</h1>
+    </div>
+    </template>
+    
+    <script>
+    export default {
+        name: 'NotFound',
+    }
+    </script>
+    
+    <style ></style>
+    
