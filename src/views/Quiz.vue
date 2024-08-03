21a9457846566db5c98c2dd96c8be2d67916b888
FelixRG <correofrg@gmail.com>
2024-08-03 10:30:15 +0200

    updated style.css

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,118 +1,80 @@
 <template>
-  <main class="flex flex-col gap-14 items-center w-full h-full relative mt-20">
-    <div class="absolute h-full w-full bg-combined-gradient"></div>
+<main class="flex flex-col gap-14 pt-20 items-center w-full h-screen  overflow-hidden">
+    <!-- background gradients decorations -->
+    <div class="absolute inset-0 bg-combined-gradient"></div>
 
     <div class="absolute top-[-50%] left-[25%] bg-gradient-radial-2 w-[50vw] h-[50vw] rounded-full blur-[350px]"></div>
-
-    <div v-if="loading || errorMessage">
-      <div class="loading">Loading...</div>
-      <span class="error">{{ errorMessage }}</span>
-    </div>
-
-    <!-- Progress Bar -->
-    <div v-if="quizData" class="w-full max-w-[80vw] h-10 bg-[#eb8ed876] rounded-t-lg absolute  -top-[64] z-50">
-      <div
-        class="bg-[#68ff1dbe] h-full rounded-t-lg transition-all duration-300"
-        :style="{ width: progressBarWidth }"
-      ></div>
-      <span class="absolute inset-0 flex items-center justify-center text-white">
-        Correct answers: {{ countCorrectAnswers }} of {{ quizData.length }}
-      </span>
+<!-- container Loading and Error message -->
+    <div v-if="loading || errorMessage" class="flex h-[85%]  justify-center items-center">
+        <div v-show="loading" class="text-8xl loading02">
+            <span>L</span>
+            <span>O</span>
+            <span>A</span>
+            <span>D</span>
+            <span>I</span>
+            <span>N</span>
+            <span>G</span>
+        </div>
+        <span class="text-red-500 text-2xl">{{ errorMessage }}</span>
     </div>
 
     <!-- Game -->
-    <div v-if="quizData" class="w-[80vw] min-h-[300px] bg-black/70 p-10 z-10 rounded-lg flex flex-col justify-center relative">
-      <h3 class="text-2xl text-center py-2 text-gradient">{{ currentQuestion.question }}</h3>
-      <span class="absolute bottom-2 right-[40%] text-[#1ffffb] animate-pulse" v-if="multipleCorrectAnswers">
-        Select all the correct answers
-      </span>
-      <span class="absolute bottom-5 right-5">Question {{ currentQuiz }}</span>
-    </div>
-
+    <div v-if="quizData" class="w-[80vw] min-h-[300px] bg-black/70 p-10 z-10 rounded-lg flex flex-col justify-center relative ">
+        <!-- Progress Bar -->
+        <div class="w-full max-w-[80vw] h-10 border-t-2 border-[#e08eeb76] rounded-t-lg absolute top-0 left-0 z-50 ">
+            <div class="bg-[#68ff1dbe] h-full rounded-t-lg transition-all duration-300" :style="{ width: progressBarWidth }"></div>
+            <span class="absolute inset-0 flex items-center justify-center text-white">
+                Correct answers: {{ countCorrectAnswers }} of {{ quizData.length }}
+            </span>
+        </div>
+        <!-- Questions -->
+        <h3 class="text-2xl text-center py-2 text-gradient">{{ currentQuestion.question }}</h3>
+        <span class="absolute bottom-2 right-[40%] text-[#1ffffb] animate-pulse" v-show="multipleCorrectAnswers">
+            Select all the correct answers
+        </span>
+        <button v-if="finalQuestion && isAnswerSelected" 
+        @click="reset"
+        class="absolute bottom-5 right-5 border-2 text-lime-400 border-lime-400 px-5 py-3 rounded-lg hover:border-[#cc00ff] hover:text-white duration-300 transition-colors">Reset Quiz</button>
+        <span v-else class="absolute bottom-5 right-5">Question {{ currentQuiz +1 }}</span>
+      </div>
+    <!-- answers -->
     <div v-if="quizData" class="z-10 w-[70vw]">
-      <ul class="grid grid-cols-2 z-10 gap-x-9 gap-y-2">
-        <li v-for="(answer, index) in filteredAnswers(currentQuestion.answers)" :key="index">
-          <button
-            class="py-5 px-5 rounded-xl border-2 border-transparent flex min-w-[50%] duration-300 hover:-translate-y-2 hover:scale-110 hover:translate-x-4"
-            :class="buttonClass(index)"
-            @click="selectAnswer(index)"
-            :disabled="isAnswerSelected"
-          >
-            <div class="flex-shrink-0">{{ `${id_Answers[index]} ) ` }}</div>
-            <span class="pl-3">{{ answer }}</span>
-          </button>
-        </li>
-      </ul>
+        <ul class="grid grid-cols-2 z-10 gap-10">
+            <li v-for="(answer, index) in filteredAnswers(currentQuestion.answers)" :key="index" class="flex ">
+                <button class="py-5 px-5 rounded-xl border-2 border-white flex-grow flex items-center duration-300  " :class="isAnswerSelected ? buttonClass(index):'hover:bg-purple-300/20'" 
+                @click="selectAnswer(index)" :disabled="isAnswerSelected">
+                    <div class="flex-shrink-0">{{ `${id_Answers[index]} ) ` }}</div>
+                    <span class="pl-3 text-start">{{ answer }}</span>
+                </button>
+            </li>
+        </ul>
     </div>
 
     <!-- Navigation Buttons -->
-    <button
-      class="text-6xl z-10 fixed right-10 top-[50%] translate-y-[-50%] opacity-80 hover:opacity-100 duration-300"
-      v-if="isAnswerSelected && quizData"
-      @click="nextQuestion"
-      :disabled="currentQuiz >= quizData.length - 1"
-    >
-      <svg
-        width="75"
-        height="75"
-        viewBox="0 0 75 75"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        transform="rotate(180)"
-      >
-        <path
-          d="M56.25 12.5H50V18.75H43.75V25H37.5V31.25H31.25V43.75H37.5V50H43.75V56.25H50V62.5H56.25V12.5ZM18.75 12.5H25V62.5H18.75V12.5Z"
-          fill="url(#paint0_linear_1)"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear_1"
-            x1="37.5"
-            y1="2.5"
-            x2="37.5"
-            y2="42.5"
-            gradientUnits="userSpaceOnUse"
-          >
-          <stop stop-color="white" />
-          <stop offset="1" stop-color="#CC00FF" />
-          </linearGradient>
-        </defs>
-      </svg>
+    <button class="text-6xl z-10 fixed right-10 top-[50%] translate-y-[-50%] opacity-80 hover:opacity-100 duration-300" v-if="isAnswerSelected && quizData" @click="nextQuestion" :disabled="currentQuiz >= quizData.length - 1">
+        <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
+            <path d="M56.25 12.5H50V18.75H43.75V25H37.5V31.25H31.25V43.75H37.5V50H43.75V56.25H50V62.5H56.25V12.5ZM18.75 12.5H25V62.5H18.75V12.5Z" fill="url(#paint0_linear_1)" />
+            <defs>
+                <linearGradient id="paint0_linear_1" x1="37.5" y1="2.5" x2="37.5" y2="42.5" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="white" />
+                    <stop offset="1" stop-color="#CC00FF" />
+                </linearGradient>
+            </defs>
+        </svg>
     </button>
 
-    <button
-      class="text-6xl z-10 left-10 top-[50%] translate-y-[-50%] fixed opacity-80 hover:opacity-100 duration-300"
-      v-if="quizData"
-      @click="lastQuestion"
-    >
-      <svg
-        width="75"
-        height="75"
-        viewBox="0 0 75 75"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        transform="rotate(360)"
-      >
-        <path
-          d="M56.25 12.5H50V18.75H43.75V25H37.5V31.25H31.25V43.75H37.5V50H43.75V56.25H50V62.5H56.25V12.5ZM18.75 12.5H25V62.5H18.75V12.5Z"
-          fill="url(#paint0_linear_2)"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear_2"
-            x1="37.5"
-            y1="12.5"
-            x2="57.5"
-            y2="92.5"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#CC00FF" />
-            <stop offset="1" stop-color="white" />
-          </linearGradient>
-        </defs>
-      </svg>
+    <button class="text-6xl z-10 left-10 top-[50%] translate-y-[-50%] fixed opacity-80 hover:opacity-100 duration-300" v-if=" (currentQuiz > 0)" @click="previousQuestion">
+        <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(360)">
+            <path d="M56.25 12.5H50V18.75H43.75V25H37.5V31.25H31.25V43.75H37.5V50H43.75V56.25H50V62.5H56.25V12.5ZM18.75 12.5H25V62.5H18.75V12.5Z" fill="url(#paint0_linear_2)" />
+            <defs>
+                <linearGradient id="paint0_linear_2" x1="37.5" y1="12.5" x2="57.5" y2="92.5" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#CC00FF" />
+                    <stop offset="1" stop-color="white" />
+                </linearGradient>
+            </defs>
+        </svg>
     </button>
-  </main>
+</main>
 </template>
 
 <script>
@@ -140,6 +102,9 @@ export default {
     computed: {
         currentQuestion() {
             return this.quizData ? this.quizData[this.currentQuiz] : {};
+        },
+        finalQuestion() {
+            return this.quizData ? this.currentQuiz === this.quizData.length - 1 : false;
         },
 
         multipleCorrectAnswers() {
@@ -196,6 +161,7 @@ export default {
                 // Si hay multiples respuestas llamamos a updateMultipleSelections() que almacena en un array las multiples selecciones
                 this.updateMultipleSelections(index);
             } else {
+
                 // Si no hay multiples respuestas llamamos a updateSingleSelection()
                 this.updateSingleSelection(index);
             }
@@ -207,6 +173,7 @@ export default {
         updateMultipleSelections(index) {
             /*  TODO: check if answer selected exist in selectedAnswers */
             // Almacenamos los indices que capturamos con los @click de los botones en un array
+            
             this.selectedAnswers.push(index);
         },
 
@@ -256,13 +223,14 @@ export default {
                     this.correctAnswers = [];
                     this.isAnswerSelected = false;
                 }
+                
                 // Actualizamos el índice de la última pregunta contestada
                 this.lastAnswered = Math.max(this.lastAnswered, this.currentQuiz);
             }
         },
 
         // Método para retroceder a la pregunta anterior
-        lastQuestion() {
+        previousQuestion() {
             // Verificamos si estamos retrocediendo
             if (this.currentQuiz > 0) {
                 this.currentQuiz--;
@@ -271,6 +239,18 @@ export default {
                 this.isAnswerSelected = true; // Desactivamos la posibilidad de cambiar respuestas
             }
         }
+        ,
+        reset(){
+          this.currentQuiz=0;
+          this.selectedAnswers = [];
+          this.correctAnswers = [];
+          this.countCorrectAnswers=0;
+          this.lastAnswered = -1;
+          this.isAnswerSelected = false;
+          this.errorMessage= null;
+          this.loading=false;
+          this.multipleCorrectAnswers= false;
+        },
     },
     mounted() {
         this.fetchQuiz();
@@ -280,50 +260,4 @@ export default {
 </script>
 
 <style>
-.loading {
-    color: blue;
-}
-
-.error {
-    color: red;
-}
-
-.selected {
-    background-color: yellow;
-}
-
-.correct {
-    border: 2px solid green;
-    color: white;
-    box-sizing: border-box;
-    /* Asegura que el borde no cambie el tamaño del botón */
-}
-
-.incorrect {
-    border: 2px solid rgb(178, 49, 49);
-    color: white;
-    box-sizing: border-box;
-    /* Asegura que el borde no cambie el tamaño del botón */
-}
-
-
-.progress {
-  height: 50px;
-  margin-top: 10px;
-  background-color: #061c29;
-  position: relative;
-}
-.bar {
-  height: 50px;
-  background-color: #721bd4;
-  transition: all 0.3s linear;
-}
-.status {
-  position: absolute;
-  top: 15px;
-  left: 0;
-  text-align: center;
-  color: #fff;
-  width: 100%;
-}
 </style>
