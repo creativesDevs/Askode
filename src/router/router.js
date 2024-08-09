53ef88292664d331d8866bd1845adcd3d8e4b885
Isiah Zacarias <isiahzacarias@gmail.com>
2024-08-09 02:02:49 +0200

    Deleted: Deleted unnecessary files and folders

--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,6 @@ import Home from "../views/Home.vue";
 import Quiz from "../views/Quiz.vue";
 import Login from "../views/Login.vue";
 import Signup from "../views/Signup.vue";
-import Forgot from "../views/Forgot.vue";
 import NotFound from "../views/NotFound.vue";
 import Categories from "../views/Categories.vue";
 import { createWebHistory, createRouter } from 'vue-router';
